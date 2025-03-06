204c6df174ab55430485c7c281617ab59094feae
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-03-06 21:03:37 +0300

    fix: update CORS origin to use environment variable for production

--- back/src/Server.ts
+++ back/src/Server.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Change this for production
+    origin: process.env.CLIENT_URL, // Change this for production
     credentials: true, // Allow cookies to be sent
   }),
 );
